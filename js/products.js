e4dba53c963a574627306a7b2345e9c762502c30
Jagadeesan <jagadmai@gmail.com>
2025-12-23 17:37:59 +0400

    k

---
+++ js/products.js
@@ -0,0 +1,30 @@
+const PRODUCTS = [
+  {
+    id: 1,
+    title: "Apple Watch Series 9",
+    price: 399,
+    image: "https://m.media-amazon.com/images/I/71LfnkRgZ4L._AC_SL1500_.jpg",
+    description: "Advanced fitness tracking and Apple ecosystem integration."
+  },
+  {
+    id: 2,
+    title: "Sony WH-1000XM5 Headphones",
+    price: 349,
+    image: "https://m.media-amazon.com/images/I/61+btxzpfDL._AC_SL1500_.jpg",
+    description: "Industry-leading noise cancellation headphones."
+  },
+  {
+    id: 3,
+    title: "Dell XPS 13 Laptop",
+    price: 999,
+    image: "https://m.media-amazon.com/images/I/61L8sY6z7WL._AC_SL1500_.jpg",
+    description: "Premium ultrabook with stunning display."
+  },
+  {
+    id: 4,
+    title: "PlayStation 5 Console",
+    price: 499,
+    image: "https://m.media-amazon.com/images/I/619BkvKW35L._AC_SL1500_.jpg",
+    description: "Next-gen gaming with ultra-fast SSD."
+  }
+];
